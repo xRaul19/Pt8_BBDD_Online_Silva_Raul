fd9a1a307f011c08eb6a13218e444f576a98658b
alumne <rasi600@vidalibarraquer.net>
2026-02-04 09:20:52 +0100

    Fet

--- app/src/main/java/com/example/pt8_bbdd_online_silva_raul/Vehicle.java
+++ app/src/main/java/com/example/pt8_bbdd_online_silva_raul/Vehicle.java
@@ -8,8 +8,6 @@ public class Vehicle {
     private String marca;
     private String model;
 
-    public Vehicle() {}
-
     public Vehicle(String matricula, String nom, String cognoms, String telefon, String marca, String model) {
         this.matricula = matricula;
         this.nom = nom;
@@ -19,16 +17,51 @@ public class Vehicle {
         this.model = model;
     }
 
-    public String getMatricula() { return matricula; }
-    public void setMatricula(String matricula) { this.matricula = matricula; }
-    public String getNom() { return nom; }
-    public void setNom(String nom) { this.nom = nom; }
-    public String getCognoms() { return cognoms; }
-    public void setCognoms(String cognoms) { this.cognoms = cognoms; }
-    public String getTelefon() { return telefon; }
-    public void setTelefon(String telefon) { this.telefon = telefon; }
-    public String getMarca() { return marca; }
-    public void setMarca(String marca) { this.marca = marca; }
-    public String getModel() { return model; }
-    public void setModel(String model) { this.model = model; }
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognoms() {
+        return cognoms;
+    }
+
+    public void setCognoms(String cognoms) {
+        this.cognoms = cognoms;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
